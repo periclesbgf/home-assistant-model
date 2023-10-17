678be6c476906db87f0aa98157053b1e74d9b3f9
Péricles Buarque <100322336+periclesbgf@users.noreply.github.com>
2023-10-16 23:31:16 -0300

    Update recording_helper.py

--- model/recording_helper.py
+++ model/recording_helper.py
@@ -16,21 +16,16 @@ def record_audio():
         frames_per_buffer=FRAMES_PER_BUFFER
     )
 
-    #print("start recording...")
-
     frames = []
     seconds = 1
     for i in range(0, int(RATE / FRAMES_PER_BUFFER * seconds)):
         data = stream.read(FRAMES_PER_BUFFER)
         frames.append(data)
 
-    # print("recording stopped")
-
     stream.stop_stream()
     stream.close()
     
     return np.frombuffer(b''.join(frames), dtype=np.int16)
-
 
 def terminate():
     p.terminate()
